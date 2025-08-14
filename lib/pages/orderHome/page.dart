19231d6d9d08259a60de8f5c7beca9198b1cbf28
Idriss019 <148658965+Idriss019@users.noreply.github.com>
2025-08-14 20:04:19 +0300

    Animation navbar

--- lib/pages/orderHome/page.dart
+++ lib/pages/orderHome/page.dart
@@ -8,150 +8,106 @@ class OrderPage extends StatefulWidget {
   State<OrderPage> createState() => _OrderPageState();
 }
 
-class _OrderPageState extends State<OrderPage> {
+class _OrderPageState extends State<OrderPage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<Offset> _animation;
+
   bool _onPressed = false;
-  void _onPress() {
-    _onPressed = !_onPressed;
-    setState(() {});
+
+  // void _onPress() {
+  //   _onPressed = !_onPressed;
+  //   _startAnimation();
+  //   setState(() {});
+  // }
+  void _toggleContainer() {
+    if (_onPressed) {
+      _controller.reverse(); // уезжает обратно
+    } else {
+      _controller.forward(); // выезжает вперед
+    }
+    setState(() {
+      _onPressed = !_onPressed; // меняем состояние
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      duration: Duration(seconds: 1),
+      vsync: this,
+    );
+
+    // Анимация сдвига слева направо
+    _animation = Tween<Offset>(
+      begin: Offset(-1.0, 0.0), // полностью слева за границей экрана
+      end: Offset(0.0, 0.0), // на месте
+    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeInOut));
+
+    // // Запускаем анимацию при инициализации
+    // _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
+      body: Column(
         children: [
-          Column(
-            children: [
-              Container(
-                color: Colors.grey,
-                child: Row(
-                  children: [
-                    IconButton(
-                      onPressed: _onPress, //_onPress,
-                      icon: Icon(Icons.settings_outlined),
-                    ),
-                    // Navbar(),
-                    Expanded(
-                      child: Container(
-                        // margin: EdgeInsets.only(left: 25, right: 25),
-                        child: Column(
-                          children: [
-                            // Expanded(
-                            //   // child: SingleChildScrollView(
-                            //   child:
-                            Column(children: [Text('Заказы')]),
-                            // ),
-                            // ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ],
+          Container(
+            color: Colors.grey,
+            child: Row(
+              children: [
+                IconButton(
+                  onPressed: _toggleContainer, //_onPress, keyboard_backspace_outlined
+                  icon: _onPressed == true ? Icon(Icons.keyboard_backspace_outlined): Icon(Icons.menu),
                 ),
-              ),
-              Container(color: Colors.blue, width: double.infinity, height: 700),
-              // Stack(
-              //   children: [
-              //     Expanded(
-              //       child: Container(
-              //         color: Colors.blue,
-              //         width: double.infinity,
-              //         height: double.infinity,
-              //       ),
-              //     ),
-              //     // Expanded(
-              //     //   child: Container(
-              //     //     color: Colors.red,
-              //     //   ),
-              //     // ),
-              //     // Positioned(
-              //     //   left: 0,
-              //     //   bottom: 0,
-              //     //   child: Visibility(
-              //     //               visible: _onPressed,
-              //     //               child: Navbar()),
-              //     // )
-              //     Visibility(
-              //       visible: _onPressed, // или условие
-              //       child: Positioned(
-              //         top: 50,
-              //         left: 50,
-              //         child: Container(color: Colors.red, width: 100, height: 100),
-              //       ),
-              //     ),
-              //   ],
-              // ),
-              // Row(
-              //   children: [
-              //     Stack(
-              //       children: [
-              //         // Другие виджеты
-              //         Container(color: Colors.blue, width: 200, height: 200),
-              //         // Контейнер, который должен появляться сверху
-              //         Visibility(
-              //           visible: _onPressed, // или условие
-              //           child: Positioned(
-              //             top: 50,
-              //             left: 50,
-              //             child: Container(
-              //               color: Colors.red,
-              //               width: 100,
-              //               height: 100,
-              //             ),
-              //           ),
-              //         ),
-              //       ],
-              //     ),
-              //   ],
-              // ),
-              
-              // Visibility(
-              //   visible: _onPressed, // или условие
-              //   child: Row(
-              //     mainAxisAlignment: MainAxisAlignment.start,
-              //     children: [
-              //       SizedBox(height: 600, width: 300, child: Navbar()),
-              //       // SizedBox(),
-                    
-              //     ],
-              //   ),
-              // ),
-              
-              
-
-              //    Visibility(
-              //   visible: _onPressed, // или условие
-              //   child: Positioned(
-              //     top: 50,
-              //     left: 0,
-              //     child: SizedBox(
-              //       height: 600,
-              //       width: 300,
-              //       child: Navbar()),
-              //   ),
-              // ),
-            ],
-          ),
-          // Container(color: Colors.blue, width: 200, height: 200),
-          // Positioned(
-          //   top: 50,
-          //           left: 50,
-            // child: 
-            Visibility(
-                  visible: _onPressed, // или условие
-                  child: Positioned(
-                    top: 40,
-                    left: 0,
-                    child: SizedBox(
-            width: 300,
-            height: 600,
-            child: Navbar(),
+                // Navbar(),
+                Expanded(
+                  child: Container(
+                    // margin: EdgeInsets.only(left: 25, right: 25),
+                    child: Column(
+                      children: [
+                        // Expanded(
+                        //   // child: SingleChildScrollView(
+                        //   child:
+                        Column(children: [Text('Заказы')]),
+                        // ),
+                        // ),
+                      ],
                     ),
                   ),
                 ),
-          // ),
+              ],
+            ),
+          ),
+          Container(
+            color: Colors.blue,
+            width: double.infinity,
+            height: 700,
+            child: Stack(
+              children: [
+                Visibility(
+                  visible: true, //_onPressed, // или условие
+                  child: SlideTransition(
+                    position: _animation,
+                    child: SizedBox(width: 300, height: 600, child: Navbar()),
+                  ),
+                ),
+              ],
+            ),
+          ),
+
         ],
       ),
+
     );
   }
 }
