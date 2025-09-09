ee74245fe123dd254cbc3ad00294cdd325d6471e
Idriss019 <148658965+Idriss019@users.noreply.github.com>
2025-09-10 00:39:13 +0300

    update create dishes

--- lib/names/creatingDishes/widget/page.dart
+++ lib/names/creatingDishes/widget/page.dart
@@ -6,6 +6,21 @@ class CreateDishesPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return NavBarWidget(child: Text('Создание блюд'));
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Navbar(
+        child: Padding(
+          padding: const EdgeInsets.only(top: 8.0),
+          child: Column(
+            children: [
+              Text(
+                'Создание блюда',
+                style: TextStyle(fontSize: 20),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/names/tables/widget/page.dart
+++ lib/names/tables/widget/page.dart
@@ -7,29 +7,132 @@ class Tables extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return NavBarWidget(child: Center(
-      child: Column(
-        children: [
-          TextButton(child: Text('Закуп'), onPressed: () {
-            context.go('/Tables/PurchasesTable');
-          },),
-          TextButton(child: Text('Продажа'), onPressed: () {
-            context.go('/Tables/SalesTable');
-          },),
-          TextButton(child: Text('Склад'), onPressed: () {
-            context.go('/Tables/StorageTable');
-          },),
-          TextButton(child: Text('Персонал'), onPressed: () {
-            context.go('/Tables/StaffTable');
-          },),
-          TextButton(child: Text('Перерасчет'), onPressed: () {
-            context.go('/Tables/Recalculation');
-          },),
-          TextButton(child: Text('Журнал действий'), onPressed: () {
-            context.go('/Tables/HistoryTable');
-          },),
-        ],
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Navbar(
+        child: Padding(
+          padding: const EdgeInsets.only(top: 8.0),
+          child: Column(
+            children: [
+              Text('Таблицы', style: TextStyle(fontSize: 40)),
+              Expanded(
+                child: ListView(
+                  children: [
+                    SizedBox(
+                      height: 90,
+                      width: double.infinity,
+                      child: TextButton(
+                        onPressed: () {
+                          context.go('/Tables/PurchasesTable');
+                        },
+                        // style: TextButton.styleFrom(
+                        //   fixedSize: const Size(300, 100),
+                        // ),
+                        child: Text('Закуп', style: TextStyle(fontSize: 25)),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 90,
+                      width: double.infinity,
+                      child: TextButton(
+                        onPressed: () {
+                          context.go('/Tables/SalesTable');
+                        },
+                        // style: TextButton.styleFrom(
+                        //   fixedSize: const Size(300, 100),
+                        // ),
+                        child: Text('Продажи', style: TextStyle(fontSize: 25)),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 90,
+                      width: double.infinity,
+                      child: TextButton(
+                        onPressed: () {
+                          context.go('/Tables/StorageTable');
+                        },
+                        // style: TextButton.styleFrom(
+                        //   fixedSize: const Size(300, 100),
+                        // ),
+                        child: Text('Склад', style: TextStyle(fontSize: 25)),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 90,
+                      width: double.infinity,
+                      child: TextButton(
+                        onPressed: () {
+                          context.go('/Tables/StaffTable');
+                        },
+                        // style: TextButton.styleFrom(
+                        //   fixedSize: const Size(300, 100),
+                        // ),
+                        child: Text('Персонал', style: TextStyle(fontSize: 25)),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 90,
+                      width: double.infinity,
+                      child: TextButton(
+                        onPressed: () {
+                          context.go('/Tables/Recalculation');
+                        },
+                        // style: TextButton.styleFrom(
+                        //   fixedSize: const Size(300, 100),
+                        // ),
+                        child: Text(
+                          'Перерасчет',
+                          style: TextStyle(fontSize: 25),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 90,
+                      width: double.infinity,
+                      child: TextButton(
+                        onPressed: () {
+                          context.go('/Tables/HistoryTable');
+                        },
+                        // style: TextButton.styleFrom(
+                        //   fixedSize: const Size(300, 100),
+                        // ),
+                        child: Text(
+                          'Журнал действий',
+                          style: TextStyle(fontSize: 25),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-    ));
+    );
+    // return NavBarWidget(child: Center(
+    //   child: Column(
+    //     children: [
+    //       TextButton(child: Text('Закуп'), onPressed: () {
+    //         context.go('/Tables/PurchasesTable');
+    //       },),
+    //       TextButton(child: Text('Продажа'), onPressed: () {
+    //         context.go('/Tables/SalesTable');
+    //       },),
+    //       TextButton(child: Text('Склад'), onPressed: () {
+    //         context.go('/Tables/StorageTable');
+    //       },),
+    //       TextButton(child: Text('Персонал'), onPressed: () {
+    //         context.go('/Tables/StaffTable');
+    //       },),
+    //       TextButton(child: Text('Перерасчет'), onPressed: () {
+    //         context.go('/Tables/Recalculation');
+    //       },),
+    //       TextButton(child: Text('Журнал действий'), onPressed: () {
+    //         context.go('/Tables/HistoryTable');
+    //       },),
+    //     ],
+    //   ),
+    // ));
   }
 }
