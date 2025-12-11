8f7d0b3309a1d5590acf87acc08198185969930c
Idriss019 <148658965+Idriss019@users.noreply.github.com>
2025-12-11 09:09:44 +0300

    new order menu

--- lib/presentation/orderHome/widget/menu_screen.dart
+++ lib/presentation/orderHome/widget/menu_screen.dart
@@ -5,21 +5,56 @@ class MenuScreen extends StatefulWidget {
   const MenuScreen({super.key});
 
   @override
-  State<MenuScreen> createState() => _MenuScreenState();
+  State<MenuScreen> createState() => MenuScreenState();
 }
 
-class _MenuScreenState extends State<MenuScreen> {
+class MenuScreenState extends State<MenuScreen> {
   MenuItem menu = MenuItem(''); // Корневой элемент меню
+  MenuItem startMenu = MenuItem(''); // Корневой элемент меню
   final String filename = 'database/menu.json';
+
+  // стек для хранения истории навигации
+  List<MenuItem> menuHistory = [];
 
   Future<void> _loadMenu() async {
     MenuItem? loadedMenu = await DirectoryFilesUtils.loadMenuFromFile(filename);
     setState(() {
-      menu =
+      startMenu =
           loadedMenu ??
           MenuItem(''); // Предполагается, что menu объявлена в State
+      menu = startMenu;
       print('Load');
     });
+  }
+
+  void refreshMenu() {
+    setState(() {
+      // _loadMenu();
+    });
+  }
+
+  void navigateTo(MenuItem newMenu) {
+    // Добавляем текущий уровень в историю перед переходом
+    menuHistory.add(menu);
+    setState(() {
+      menu = newMenu;
+    });
+  }
+
+  void goBack() {
+    if (menuHistory.isNotEmpty) {
+      setState(() {
+        menu = menuHistory.removeLast();
+      });
+    }
+  }
+
+  void goToHome() {
+    if (menuHistory.isNotEmpty) {
+      setState(() {
+        menu = startMenu;
+      });
+    }
   }
 
   @override
@@ -30,6 +65,11 @@ class _MenuScreenState extends State<MenuScreen> {
 
   @override
   Widget build(BuildContext context) {
+    // print('menu.name : ${menu.children.length}');
+    // final indexNumber = 0;
+    // // data.children.map((entry) {
+    // String nameMenu = '';
+    // MenuItem valueItem = MenuItem('');
     return Flexible(
       flex: 1,
       child: Container(
@@ -37,16 +77,58 @@ class _MenuScreenState extends State<MenuScreen> {
         decoration: BoxDecoration(color: Colors.blue),
         child: Column(
           children: [
-            Text('Меню', style: TextStyle(fontSize: 40)),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                SizedBox(
+                  width: 40,
+                  child: IconButton(
+                    onPressed: () {
+                      goToHome();
+                    },
+                    icon: Icon(Icons.home),
+                  ),
+                ),
+                Text('Меню', style: TextStyle(fontSize: 40)),
+                SizedBox(
+                  width: 40,
+                  child: IconButton(
+                    onPressed: () {
+                      goBack();
+                    },
+                    icon: Icon(Icons.arrow_back),
+                  ),
+                ),
+              ],
+            ),
             Divider(height: 2, color: Colors.black),
+            // Expanded(
+            //   child: ListView(
+            //     children: menu.children.map((item) {
+            //       return MenuItemWidget(title: item.name, menu: item);
+            //     }).toList(),
+            //   ),
+            // ),
+            // Expanded(
+            //   child: ListView(
+            //     children:   menu.children.map((item) {
+            //       return MenuItemWidget(
+            //         title: item.name,
+            //         menu: item,
+            //         isSelected: item == selectedMenu, // передача статуса
+            //         onItemSelected: () => selectMenuItem(item),
+            //       );
+            //     }).toList(),
+            //   ),
             Expanded(
               child: ListView.builder(
                 itemCount: menu.children.length,
                 itemBuilder: (BuildContext context, int index) {
                   // print(menu.children[0].name);
                   return MenuItemWidget(
-                    title: menu.children[0].name,
+                    title: menu.children[index].name,
                     menu: menu.children[index],
+                    // indexMenu: index,
                   );
                 },
               ),
@@ -97,52 +179,187 @@ class _MenuScreenState extends State<MenuScreen> {
 class MenuItemWidget extends StatefulWidget {
   final String title;
   final MenuItem menu;
+  // final int indexMenu;
 
-  const MenuItemWidget({required this.title, required this.menu, super.key});
+  const MenuItemWidget({
+    required this.title,
+    required this.menu,
+    // required this.indexMenu,
+    super.key,
+  });
 
   @override
   MenuItemWidgetState createState() => MenuItemWidgetState();
 }
 
 class MenuItemWidgetState extends State<MenuItemWidget> {
-  bool isExpanded = false;
-
+  // @override
+  // void didUpdateWidget(covariant MenuItemWidget oldWidget) {
+  //   super.didUpdateWidget(oldWidget);
+  //   if (oldWidget.isReset != widget.isReset && widget.isReset) {
+  //     // Когда флаг сброса меняется на true, скрываем дочерние
+  //     setState(() {
+  //       isExpanded = false;
+  //     });
+  //   }
+  // }
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
+        if(widget.menu.isDirectory)
         SizedBox(
           height: 70,
           width: double.infinity,
           child: TextButton(
-            onPressed: () {
-              if (widget.menu.isDirectory) {
-                setState(() {
-                  isExpanded = !isExpanded; // переключение состояния
-                });
-              }
-            },
-            child: Text(
-              widget.title,
-              style: TextStyle(fontSize: 25),
-            ),
-          ),
-        ),
-        if (isExpanded)
-          ListView.builder(
-            shrinkWrap: true,
-            physics: ClampingScrollPhysics(),
-            itemCount: widget.menu.children.length,
-            itemBuilder: (BuildContext context, int index) {
-              final childMenu = widget.menu.children[index];
-              return MenuItemWidget(
-                title: childMenu.name,
-                menu: childMenu,
-              );
-            },
-          ),
+                onPressed: () {
+                  final parentState = context.findAncestorStateOfType<MenuScreenState>();
+                  if (widget.menu.children.isNotEmpty) {
+                    parentState?.navigateTo(widget.menu);
+                  }
+                },
+                child: Text(widget.title, style: TextStyle(fontSize: 25)),
+              )
+        )else Container(
+          color: Colors.grey,
+          height: 100,
+          width: double.infinity,
+          child: TextButton(
+                onPressed: () {
+                  final parentState = context.findAncestorStateOfType<MenuScreenState>();
+                  if (widget.menu.children.isNotEmpty) {
+                    parentState?.navigateTo(widget.menu);
+                  }
+                },
+                child: Text(widget.title, style: TextStyle(fontSize: 25)),
+              ) // пустой виджет, если условие не выполнено
+        )
+        //   TextButton(
+        //     onPressed: () {
+        //       final parentState = context
+        //           .findAncestorStateOfType<MenuScreenState>();
+        //       if (widget.menu.children.isNotEmpty) {
+        //         // Переход на выбранное подменю
+        //         parentState?.navigateTo(widget.menu);
+        //       }
+        //     },
+        //     child: Text(widget.title, style: TextStyle(fontSize: 25)),
+        //   ),
+        // ),
+        // if (isExpanded && !widget.isReset)
+        //   ListView.builder(
+        //     shrinkWrap: true,
+        //     physics: ClampingScrollPhysics(),
+        //     itemCount: widget.menu.children.length,
+        //     itemBuilder: (BuildContext context, int index) {
+        //       // resetWidget = false;
+        //       final childMenu = widget.menu.children[index];
+        //       return MenuItemWidget(
+        //         isReset: widget.isReset,
+        //         title: childMenu.name,
+        //         menu: childMenu,
+        //         isSelected:
+        //             widget.menu.isDirectory &&
+        //             (childMenu == widget.menu.children[index]),
+        //         onItemSelected: widget.onItemSelected,
+        //       );
+        //     },
+        //   ),
       ],
     );
   }
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   return Column(
+  //     children: [
+  //       SizedBox(
+  //         height: 70,
+  //         width: double.infinity,
+  //         child: TextButton(
+  //           onPressed: () {
+  //             if (widget.menu.isDirectory) {
+  //               // при нажатии скрывать/показывать
+  //               setState(() {
+  //                 isExpanded = !isExpanded;
+  //               });
+  //             } else {
+  //               // при выборе файла - закрывать всё и запускать что надо
+  //               widget.onItemSelected();
+  //               // Можно дополнительно "запустить" или обновить состояние
+  //             }
+  //           },
+  //           child: Text(widget.title, style: TextStyle(fontSize: 25)),
+  //         ),
+  //       ),
+  //       if (isExpanded)
+  //         ListView.builder(
+  //           shrinkWrap: true,
+  //           physics: ClampingScrollPhysics(),
+  //           itemCount: widget.menu.children.length,
+  //           itemBuilder: (BuildContext context, int index) {
+  //             final childMenu = widget.menu.children[index];
+  //             return MenuItemWidget(
+  //               title: childMenu.name,
+  //               menu: childMenu,
+  //               isSelected:
+  //                   widget.menu.isDirectory &&
+  //                   (childMenu == widget.menu.children[index]),
+  //               onItemSelected: widget.onItemSelected,
+  //             );
+  //           },
+  //         ),
+  //     ],
+  //   );
+  // }
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   return Column(
+  //     children: [
+  //       SizedBox(
+  //         height: 70,
+  //         width: double.infinity,
+  //         child: TextButton(
+  //           onPressed: () {
+  //             if (widget.menu.isDirectory) {
+  //               setState(() {
+  //                 isExpanded = !isExpanded; // переключение состояния
+  //               });
+  //             }
+  //             if (widget.menu.isDirectory == false) {
+  //               // isExpanded = !isExpanded;
+  //               print('Press False!');
+  //               // setState(() {
+  //               //   isExpanded = !isExpanded; // переключение состояния
+  //               // });
+  //             }
+  //           },
+  //           child: Text(widget.title, style: TextStyle(fontSize: 25)),
+  //         ),
+  //       ),
+  //       if (isExpanded)
+  //         ListView.builder(
+  //           shrinkWrap: true,
+  //           physics: ClampingScrollPhysics(),
+  //           itemCount: widget.menu.children.length,
+  //           itemBuilder: (BuildContext context, int index) {
+  //             final childMenu = widget.menu.children[index];
+  //             return MenuItemWidget(title: childMenu.name, menu: childMenu);
+  //           },
+  //         ),
+  //       // if (isExpanded == false)
+  //       // ListView.builder(
+  //       //   shrinkWrap: true,
+  //       //   physics: ClampingScrollPhysics(),
+  //       //   itemCount: widget.menu.children.length,
+  //       //   itemBuilder: (BuildContext context, int index) {
+  //       //     final childMenu = widget.menu.children[index];
+  //       //     return MenuItemWidget(title: childMenu.name, menu: childMenu);
+  //       //   },
+  //       // ),
+  //     ],
+  //   );
+  // }
 }
